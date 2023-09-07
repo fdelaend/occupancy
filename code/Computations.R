2dc353f145ef01a73d6933390bd28750839b5d36
Frederik De Laender <frederik.delaender@unamur.be>
2023-09-07 08:56:26 +0200

    Added code and figures

---
+++ code/Computations.R
@@ -0,0 +1,131 @@
+extinctionThreshold <- 1e-3
+
+# CALCULATIONS ------
+data <- expand_grid(n = 6, meanA = c(0, 0.2, 0.4, 0.8), 
+                    d = seq(-6,-4, length.out=4),
+                    sdA = 0, p = 50, rep = c(1:3)) %>% #nr of species, mean and cv of a, nr of patches in landscape; nr of reps
+  #Make parameters
+  mutate(d = 10^d) %>%
+  mutate(A = pmap(., function(meanA, sdA, n, ...) 
+    matrix(data = rnorm(n^2, meanA, sdA), ncol = n))) %>% 
+  mutate(A = map(A, ~make_symmetric(.x))) %>% #make symmetric and ditch diagonal
+  mutate(A = map(A, ~ set_diagonal(A=.x, d=1))) %>% #set self to 1
+  mutate(A = pmap(., make_block_diagonal)) %>% #make A spatial
+  mutate(R = pmap(., make_R_spatial)) %>% #make spatial R (note that the emigration is subtracted later so this is the real local R)
+  mutate(D = pmap(., make_D)) %>% #make dispersal matrix
+  mutate(N0 = map(R, ~ .x*0+extinctionThreshold)) %>% #set initial conditions
+  #Simulate the network
+  mutate(NHat = pmap(., get_NHat)) %>%
+  #Add R for later analysis
+  mutate(NHat = map2(NHat, R, ~ .x %>% mutate(R=.y))) %>%
+  #Analyse: 
+  #0/cv of R within a patch
+  mutate(cvR = map(NHat, ~ .x %>% group_by(location) %>% summarize(cvR = sd(R)/mean(R)))) %>%
+  #1/nr of feasible patches (aka where all present)
+  mutate(nrFeas = pmap_dbl(., function(NHat, n,...) {
+    NHat %>% mutate(present = density>extinctionThreshold) %>%
+      group_by(location) %>%
+      summarize(nrSp = sum(present)) %>%
+      filter(nrSp==n) %>% nrow()})) %>%
+  mutate(propFeas = nrFeas/p) %>%
+  #2/total density across all patches and species
+  mutate(NTot = map_dbl(NHat, ~ sum(.x["density"]))) %>%#total density across all patches
+  #3/mean density of each species across all patches  
+  mutate(meanAcross = pmap(., function(NHat,...) {
+    NHat %>% 
+      group_by(sp) %>%
+      summarize(meanNAcross = mean(density))})) %>%
+  #4/mean density at each site, across all species
+  mutate(meanWithin = pmap(., function(NHat,...) {
+    NHat %>% 
+      group_by(location) %>%
+      summarize(meanNWithin = mean(density))})) %>%
+  #5/total of local interactions experienced by every species: R - AN, 
+  # after having set diag(A) to zero (because we only want interactions with heterospecifics)
+  mutate(ANoti = map(A, ~ set_diagonal(A=.x, d=0))) %>% #set self to 0
+  mutate(X = map2(NHat, ANoti, ~ c(.x$R)-.y%*%c(.x$density))) %>%
+  # add info to NHat solution
+  mutate(NHat = map2(NHat, X, ~ .x %>% mutate(X=.y[,1]))) %>%
+  mutate(XMean = map(NHat, ~ .x %>% group_by(location) %>% 
+                       summarize(XMean=mean(X^2))))
+
+# PLOTS ------
+ggplot(data) + 
+  scale_color_gradient(low = "yellow", high = "red") +
+  theme_bw() +
+  aes(x=log10(d), y=propFeas, col=meanA) + 
+  geom_point() +
+  labs(x=expression(paste("log"[10],"(d)")), 
+       y="Patch occupancy (fraction)", col="a")
+# There is a region of d where it has a strong influence on patch occupancy
+# while it does not yet influence total density.
+
+data_with_prediction <- data %>%
+  ungroup() %>%
+  mutate(propFeas_pred = (2*(-atan(meanA/1) + atan(1/meanA))/pi)) %>%
+  mutate(a = meanA) %>%
+  unite("it", rep, a)
+
+ggplot(data_with_prediction) + 
+  theme_bw() +
+  scale_color_gradient(low = "yellow", high = "red") +
+  scale_linetype_manual(values=rep("solid", 1000)) + 
+  aes(x=log10(d), y=propFeas, col=meanA) + 
+  geom_jitter(height = 0, width = 0.1) +
+  geom_line(aes(x=log10(d), y=propFeas_pred, col=meanA, lty=it),
+            show.legend = F) +
+  labs(x=expression(paste("log"[10],"(d)")), 
+       y="Patch occupancy (fraction)", col="a")
+
+ggsave(paste0("../figures/feas.pdf"), width=4, height = 3, 
+       device = "pdf")
+
+## Does a well-mixed system behave as a single system?
+# No
+# But switching off dispersal gives same pattern,
+# so probably only due to the fact that we have many patches, 
+# some of which contain both, others only 1, still others only 2. 
+ggplot(data %>% filter(d==10^-6)) + 
+  scale_color_gradient(low = "yellow", high = "red") +
+  theme_bw() +
+  aes(x=`1`, y=`1single`, col=meanA) + 
+  geom_point() + 
+  labs(x="total density sp 1 across network", 
+       y="density of sp 1 in one mean system") + 
+  geom_abline(slope = 1, intercept = 0)
+
+## Do patches with two species contain more density than pathces with one?
+density_per_patch <- data %>%
+  select(all_of(c("n", "meanA", "d", "totalPerSite"))) %>%
+  unnest(totalPerSite)
+
+ggplot(density_per_patch) + 
+  scale_color_gradient(low = "yellow", high = "red") +
+  theme_bw() +
+  aes(x=nrSp, y=NTotal_loc, col=meanA) +
+  geom_point()
+  
+## Can we predict the total density across the whole network 
+## with a model that acts as if all were a single system with summed Rs as the R?
+ggplot(data) + 
+  scale_color_gradient(low = "yellow", high = "red") +
+  theme_bw() +
+  aes(x=NsingleTotal, y=NTot, col=d) + 
+  geom_abline(intercept = 0, slope = 1) +
+  geom_point()
+#No!
+
+# LEFTOVERS ---------
+  #5/model equilibrium as if all were a single system with summed Rs as the R
+  mutate(Rsingle = pmap(., function(R, n, p,...) {
+    tibble(R) %>% 
+      mutate(sp = rep(c(1:n), p)) %>%
+      group_by(sp) %>%
+      summarize(R = sum(R)) %>% #total across whole network
+      select(R) %>%
+      as_vector()})) %>%
+  mutate(Nsingle = map2(meanA, Rsingle, ~ solve(diag(length(.y))+.x-diag(length(.y))*.x)%*%.y)) %>%
+  #get total density as such obtained
+  mutate(NsingleTotal = map_dbl(Nsingle, ~ ifelse(prod(.x>0), sum(.x), NA))) %>%
+  #just pick one species 
+  mutate(`1single` = map_dbl(Nsingle, ~ifelse(prod(.x>0), .x[1], NA)))
---
+++ code/Tools.R
@@ -0,0 +1,122 @@
+library(deSolve)
+library(mvtnorm)
+library(tidyverse)
+library(igraph)
+library(pracma)
+library(gridExtra)
+cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", 
+               "#0072B2", "#D55E00", "#CC79A7")#
+
+# Functions ------------------------------------------------------------
+## To make all sorts of matrices -------------------------------------
+#Make block diagonal matrix by stacking p times A
+# on the diagonal. The matrix A can be simply perfectly copied (vary=0),
+# or varied to some extend among copies, as quantified by  
+# "vary" = the width of the uniform (max-min) around the elements of A
+make_block_diagonal <- function(A, p, vary = 0, ...) {
+  A_temp <- array(0, dim=dim(A)*p) # all zeros
+  for (i in 1:p) {
+    A_vary <- array(data = runif(prod(dim(A)), 1 - vary, 1 - vary), dim = dim(A)) #local deviation of A in block i
+    A_temp[(ncol(A)*i-ncol(A)+1):(ncol(A)*i), (ncol(A)*i-ncol(A)+1):(ncol(A)*i)] <- A * A_vary
+    }
+  return(A_temp)
+}
+
+set_diagonal <- function(A, d){
+  diag(A) <- d
+  A
+}
+
+#Make a dispersal (D) matrix
+# all diagonal blocks are zero
+# off diagonals blocks are diagonal matrices containing the sp-specific dispersal rates
+# if two sites are connected, they exchange individuals in both directions (but allowing for different rates)
+# d = dispersal rate 
+# p = nr of locations
+# n = nr of sp
+# connectivity = *fraction* of all possible combos that are connected
+make_D <- function(p=3, n=2, connectivity=1, d=1e-3, ...){
+  D    <- array(0, dim=c(n*p, n*p))#matrix w correct dimensions and all zeros
+  pos_links<- combn(c(1:p), 2) # all possible combos
+  nr_links <- round(connectivity * ncol(pos_links), 0) #nr of links between locations
+  links    <- pos_links[,sample(c(1:ncol(pos_links)), nr_links)]#sample that nr of links
+  for (i in 1:ncol(links)) {
+    D_12 <- diag(rep(d, n))#make random dispersal matrix from 1 to 2
+    D_21 <- diag(rep(d, n))#make random dispersal matrix from 2 to 1
+    pos_1 <- (n*links[1,i]-n+1):(n*links[1,i]) #position of first location
+    pos_2 <- (n*links[2,i]-n+1):(n*links[2,i]) #position of second location
+    D[pos_1, pos_2]<-D_12
+    D[pos_2, pos_1]<-D_21
+  }
+  return(D)
+}
+
+#make symmetric and ditch diagonal
+make_symmetric <- function(A){
+  A*upper.tri(A) + t(A)*lower.tri(A)
+}
+
+#generates p intrinsic growth rate vectors, stacked underneath each other
+#for use in spatial LV simulations
+#negative_sign is a vector telling which species needs to have a negative intrinsic growth rate
+#example: c(2,3) is the case where the intrinsic growth rate of sp 2 and 3 carry a negative sign
+#If you want none to have a negative r, just go negative_sign= some i>n
+#n is nr of species, p is nr of patches
+make_R_spatial <- function(n, p, negative_sign=10000, ...){
+  #sample from a sphere to get the directions right
+  rawRs <- t(sample_sphere_surface(dim=n, n = p, radius = 1, positive = TRUE))
+  #now make sure the mean r across species is 1 at all patches
+  rawRs <- diag(1/rowMeans(rawRs)) %*% rawRs 
+  #abd put everything into a nice format
+  Rs     <- as_tibble(rawRs)%>%
+    rename_with(~gsub("V","", .x, fixed = TRUE)) %>% #give correct sign to intrinsic growth rate of consumer
+    mutate(location=c(1:p)) %>%
+    pivot_longer(!location) %>%
+    mutate(value=value*(-1)*name%in%negative_sign + value*(1-name%in%negative_sign))
+  return(Rs$value)
+}
+
+## LV -------------------------------------
+
+#Cut-off function to limit the values of the state to enhance 
+#numerical stability when solving the odes
+cutoff <- function(n, a) {
+  return(1*(n<a)*(3*(n/a)^2-2*(n/a)^3)*ifelse(n<0, 0, 1)+ifelse(n<a, 0, 1))
+}
+
+#the LV model; R= a vector of max. intrinsic growth rates; 
+#A= matrix with linear species interactions, global var 
+run_LV <- function(time, state, pars) {
+  return(list(as.numeric(
+    ((state*(pars$R+pars$A%*%state)))*cutoff(state, 1e-10)
+  )))
+}
+
+#Spatial LV model; R= a vector of max. intrinsic growth rates; 
+#A= matrix with linear species interactions, D=dispersal matrix
+run_LV_spatial <- function(time, state, pars) {
+  return(list(as.numeric(
+    ((state*((pars$R-colSums(pars$D))-pars$A%*%state))+pars$D%*%state)*
+      cutoff(state, 1e-10)
+  )))
+}
+#compute NHat from spatial LV simulation
+#n = nr of sp
+#R, A, D: as in run_LV_spatial
+#max_time = max simulation time
+
+get_NHat <- function(n, R, A, D, max_time=100, N0, ...){
+  p         <- length(R)/n #infer nr of patches
+  densities <- ode(func=run_LV_spatial, y=N0, 
+                   parms=list(R=R, A=A, D=D), 
+                   times=seq(1,max_time,0.1))
+  locations <- NULL; for (i in 1:p) {locations <- c(locations, rep(i, n))}
+  return(as_tibble(cbind(density=tail(densities, 1)[-1], 
+                         location=locations,
+                         sp=rep(c(1:n), p)))) 
+}
+
+
+
+
+
