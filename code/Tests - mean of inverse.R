b64fc01eac1eeca394dc2cceb9a4b827a4fb106b
Frederik De Laender <frederik.delaender@unamur.be>
2023-12-04 19:03:58 +0100

    playing with matrices

--- code/Tests - mean of inverse.R
+++ code/Tests - mean of inverse.R
@@ -1,17 +1,19 @@
 
-data <- expand_grid(n = c(5, 10, 20, 40, 80), meanA = c(0), sdA = 0.5) %>% 
+data <- expand_grid(n = c(5, 10, 20, 40, 80, 160), meanA = c(0.1), sdA = 0.1,
+                    rep=c(1:10)) %>% 
   #Make parameters
   mutate(A = pmap(., function(meanA, sdA, n, ...) 
     matrix(data = rnorm(n^2, meanA, sdA), ncol = n) %>% #make symmetric and ditch diagonal; set self to 1
-      set_diagonal(d=1))) %>%
+      make_symmetric() %>% set_diagonal(d=1))) %>%
   mutate(AInv = map(A, ~solve(.x)),
          meanDiagAInv = map_dbl(AInv, ~mean(diag(.x))),
-         meanOffDiagInvTimesN = n*map_dbl(AInv, ~1/(nrow(.x)*(nrow(.x)-1))*sum(.x - diag(x=diag(.x), nrow=nrow(.x)))))
+         meanOffDiagInv = map2_dbl(AInv, n, ~sum(.x*(1-diag(.y)))/(.y^2-.y)),
+         feas = map2_dbl(AInv, n, ~prod((.x%*%rep(1,.y))>0)))
 
 ggplot(data) + 
-  aes(x=n, y=meanDiagAInv) + 
+  aes(x=log10(n), y=meanDiagAInv) + 
   geom_point() + 
-  geom_point(aes(x=n, y=meanOffDiagInvTimesN), col="red")
+  geom_point(aes(x=log10(n)+0.01, y=meanOffDiagInv), col="red")
 
 
 
