e943325b1d327fecfcc1db86f029b6715428b6bf
Frederik De Laender <frederik.delaender@unamur.be>
2024-04-30 19:14:50 +0200

    further tinkering

--- code/MVN.py
+++ code/MVN.py
@@ -10,7 +10,7 @@ def feasibility(A):
   V = np.linalg.inv(A)
   S=len(V[0,:])
   bounds=np.zeros(S)
-  cov=100*np.dot(V,V.T)  #a factor 100 to spread out the distribution (somehow helps). 
+  cov=np.dot(V,V.T)  #a factor 100 to spread out the distribution (somehow helps). 
   mean=np.zeros(S)
   return 2**S*multivariate_normal.cdf(bounds,mean,cov, allow_singular=True)
  
--- code/TestMVN.R
+++ code/TestMVN.R
@@ -3,14 +3,13 @@ get_fraction_m_alt <- function(meanA=0.5, m=2, n=3, ...){
   #n x n matrix
   A   <- diag(n) + meanA
   A   <- set_diagonal(A, 1)
-  Am <- A %*% diag(c(rep(1,m),rep(0,n-m)))
-  #Am2<- diag(c(rep(1,m),rep(0,n-m))) %*% Am
-  diag(Am) <- c(rep(1,m), rep(-1,n-m)) 
+  #Am <- A %*% diag(c(rep(1,m),rep(0,n-m)))
+  #diag(Am) <- c(rep(1,m), rep(-1,n-m)) 
   #diag(Am2)<- 1
-  B        <- diag(c(rep(1, n))) #constraints
-  #feas1 <- 2^(n)*calculate_omega_constraint(A=Am, B=B)^(n)
-  feas1 <- 2^(n)*calculate_omega(Am)^(n)
-  print(feas1)
+  #B        <- diag(c(rep(1, n))) #constraints
+  #feas1 <- calculate_omega_constraint(A=Am, B=B)^(n)
+  #feas1 <- calculate_omega_constraint(A=Am, B=B)^(m)
+  #feas1 <- calculate_omega(A)^(n)
   Ainv <- solve(A)
   # Calculate and partition mu and sigma
   mu <- c(Ainv%*%rep(0, n))
@@ -22,11 +21,15 @@ get_fraction_m_alt <- function(meanA=0.5, m=2, n=3, ...){
   sigma22 <- sigma[c((m+1):n), c((m+1):n)]
   muBar <- mu1 + sigma12 %*% solve(sigma22) %*% (0-mu2)
   sigmaBar <- sigma11 - sigma12 %*% solve(sigma22) %*% sigma21
-  feas2 <- 2^n*pmvnorm(lower=rep(0,m), upper=Inf, mean=as.vector(muBar), 
-                   sigma=sigmaBar)
-  #feas2 <- (2^n)*pmvnorm(lower=rep(0,n), upper=Inf, mean=as.vector(mu), 
+  #feas2 <- 
+  choose(n, m) * pmvnorm(lower=rep(0,m), upper=Inf, mean=as.vector(muBar), sigma=sigmaBar)[[1]]
+  #feas2 <- pmvnorm(lower=rep(0,n), upper=Inf, mean=as.vector(mu), 
   #                 sigma=sigma)
-  print(feas2)
+  #print(feas1)
+  #print("=")
+  #print(feas2)
+  #print("=")
+  #print(feasibility(A)/(2^n))
 }
 
 
