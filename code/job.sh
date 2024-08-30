dc605c4375c94394397bd9ab54a40933be10e6b3
Frederik De Laender <frederik.delaender@unamur.be>
2024-08-30 16:18:59 +0200

    Update job

--- code/job.sh
+++ code/job.sh
@@ -6,7 +6,7 @@
 #
 #SBATCH --ntasks=1 
 #SBATCH --mem-per-cpu=500 # megabytes 
-#SBATCH --partition=batch,long
+#SBATCH --partition=batch
 #
 #SBATCH --mail-user=frederik.delaender@unamur.be
 #SBATCH --mail-type=ALL
