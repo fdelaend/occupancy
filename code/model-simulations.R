32c6ea27ae9e417064870226f8d6b90c61b870c6
Frederik De Laender <frederik.delaender@unamur.be>
2024-09-24 15:24:46 +0200

    Adjustment simulation script
    Changed parts back to %>% to avoid issues on R version on Hercules

--- code/model-simulations.R
+++ code/model-simulations.R
@@ -29,27 +29,27 @@ Sims <- expand_grid(n = c(6), meanA = c(0.2, 0.4, 0.8, 1), #, 6; 0.4, 0.4, 0.6,
   mutate(N0 = map(R, ~.x/10)) |> #set initial conditions equal to carrying capacity w/o interactions or dispersal, divided by 10
   pivot_longer(cols = c("regularD","exponentialD"), names_to = "dispType", values_to = "D") |> # pivot the two sorts of D matrices to make them a factor
   #Simulate the network
-  (\(x) mutate(x, NHat = pmap(x, get_NHat)))() |>
+  (\(x) mutate(x, NHat = pmap(x, get_NHat)))() %>%
   #1/ Summarize the simulated data: per m, compute the nr of patches and total biomass of an average patch
-  (\(x) mutate(x, summaryM = pmap(x, \(NHat, R, n,...) 
-                                  NHat |>
-                                    mutate(present = density>extinctionThreshold,
-                                           R=R) |>
-                                    summarize(m = as_factor(sum(present)),
-                                              NTotal = sum(density),
-                                              meanRPer = sum(R*present)/sum(present), 
-                                              .by = location) |>
-                                    mutate(m = fct_expand(m, as.character(c(1:n)))) |>
-                                    group_by(m, .drop=F) |>
-                                    summarize(nrPatches = n(),#nr of patches with m sp.
-                                              NTotal = mean(NTotal),#total biomass in a patch with m sp.
-                                              meanRPer = mean(meanRPer)))))()  |>#mean r of persisting sp.
+  mutate(summaryM = pmap(., function(NHat, R, n,...){
+    NHat %>%
+      mutate(present = density>extinctionThreshold,
+             R=R) %>%
+      group_by(location) %>%
+      summarize(m = as_factor(sum(present)),
+                NTotal = sum(density),
+                meanRPer = sum(R*present)/sum(present)) %>%
+      mutate(m = fct_expand(m, as.character(c(1:n)))) %>%
+      group_by(m, .drop=F) %>%
+      summarize(nrPatches = n(),#nr of patches with m sp.
+                NTotal = mean(NTotal),#total biomass in a patch with m sp.
+                meanRPer = mean(meanRPer))})) %>%
   #2/proportion of patches in which all n species persist
-  mutate(propPatchesN = 1/p*map2_dbl(summaryM, n, ~ (.x |> filter(m==.y))$nrPatches)) |>
+  mutate(propPatchesN = 1/p*map2_dbl(summaryM, n, ~ (.x %>% filter(m==.y))$nrPatches)) %>%
   #3/total density across all patches of a species
-  mutate(NTotalK = map(NHat, ~ .x |> 
-                         summarize(NTotalK = sum(density), .by = sp))) |>
+  mutate(NTotalK = map(NHat, ~ .x %>%
+                         group_by(sp) %>%
+                         summarize(NTotalK = sum(density)))) %>%
   select(!A & !D & !distances & !N0 & !coords) #ditch all unneeded data
-
 
 saveRDS(Sims, file=paste("../simulated-data/",parallel_id,"data.RDS",sep=""))
