8087cff4e17f506bac338fb876d6a68e0f7f9d7a
Frederik De Laender <frederik.delaender@unamur.be>
2025-02-11 21:23:50 +0100

    First attempt at poisson glm

--- code/daphnid-analysis.R
+++ code/daphnid-analysis.R
@@ -4,15 +4,18 @@ library(broom)
 library(lme4)
 
 # READ DATA ----
-island_measures <- read_csv("../data/Ebert/Frederik_data1/Island_measures_vers4.csv") |>
-  select(island, group_100, group_200) |>
+cluster_measures <- read_csv("../data/Ebert/Frederik_data1/Island_measures_vers4.csv") |>
   mutate(cluster = factor(group_100)) |>
-  select(island, cluster)
+  mutate(nr_pools = sum(number_pools_approx),
+            area = sum(island_area_sq_meter),
+            peri = sum(island_perimeter_meter),
+            .by = cluster) |>
+  mutate(pools_dens = nr_pools / peri) |>
+  select(cluster, island, nr_pools, peri, pools_dens)
 
 counts          <- read_csv("../data/Ebert/Frederik_data1/Daphnia_dynamics_1982_2017_2.csv") |>
-  left_join(island_measures, by="island")
+  left_join(cluster_measures, by="island")
 
-#desiccation     <- read_csv("../data/Ebert/Frederik_data1/Data_hydroperiod_Means.csv") |>
 desiccation_static <- read_csv("../data/Ebert/Frederik_data1/Data_hydroperiod_Means.csv") |>
   expand_grid(year = c(min(counts$year):max(counts$year)))
 
@@ -20,7 +23,7 @@ desiccation        <- desiccation_static |>
   left_join(read_csv("../data/Ebert/hydro.csv"), 
             by = join_by(poolname == pool, year)) |> 
   separate_wider_delim(poolname, delim = "-", names = c("island", "pool")) |>
-  left_join(island_measures, by="island") |>
+  left_join(cluster_measures, by="island") |>
   summarise(desiccation_dynamic  = mean(predicted_desiccation_events, na.rm = T),
             desiccation_static_mean_mean   = mean(meandesi, na.rm = T),
             desiccation_static_mean_median = mean(mediandesi, na.rm = T),
@@ -28,146 +31,44 @@ desiccation        <- desiccation_static |>
             desiccation_static_median_mean = median(meandesi, na.rm = T),
             .by = c(year, cluster))
 
-ggplot(desiccation) +
-  scale_color_viridis_d(option="plasma", end=0.9) +
-  aes(x = desiccation_static_mean_mean, y = desiccation_dynamic, 
-      col = as.factor(year)) +
-  geom_point() +
-  geom_smooth(method = "lm", se = F)
+#environmental variables
+pH_conductivity <- read_csv("../data/Ebert/Frederik_data1/MetapopData_pH_conductivity_1998_2017.csv") |>
+  # compute mean data across sampling occasions for every pool 
+  summarize(pH = log10(mean(10^pH, na.rm = T)), 
+            log_conduct = mean(log10(conduct_uS), na.rm = T), 
+            .by = c("year", "island", "pool"))
 
-#location of pools
-pools           <- read_csv("../data/Ebert/Frederik_data1/Pools_coordinates_2017_vers7.csv") |>
-  left_join(island_measures, by="island") |>
-  select(island, pool, latitude_corr, longitude_corr, cluster) 
-
-# add desiccation to data and 
-# compute richness and fraction
-fractions <- counts |>
+# add environmental data to count data
+counts_env <- counts |>
   mutate(richness = magna + longispina + pulex) |>
-  #remove empty pools
-  filter(richness > 0) |>
-  #total nr of pools per cluster, taken from pools file
-  left_join(pools |> summarise(p = length(pool), .by = cluster),
-            by = join_by(cluster)) |>
-  #number of pools evaluated in a given cluster at a given sampling event in a given year
-  mutate(n = length(unique(poolname)), 
-         .by = c(cluster, sample, year)) |> 
-  #remove all cluster - sampling event - year combos that have too small n
-  filter(n > 10) |>
-  #the weight of a single pool is 1/n. 
-  #Making the sum across richness levels seems a tidy way to get the fraction of pools per richness level
-  mutate(weight = 1/n) |>
-  #fraction of pools with given richness level
-  summarise(fraction = sum(weight), 
-            .by = c(cluster, p, sample, year, richness, n)) |> #, "author"
   #lag one year to join with correct desiccation data in next step
-  mutate(year = year - 1) |>
+  mutate(year_lag = year - 1) |>
   #add desiccation data
-  left_join(desiccation, by = join_by(cluster, year)) |>
+  left_join(desiccation, by = join_by(cluster, year_lag == year)) |>
+  #add pH and cond 
+  left_join(pH_conductivity, by = join_by(island, pool, year)) |>
   #remove na-s
-  filter(!is.na(desiccation_dynamic))
+  filter(!is.na(richness))
 
-# CO-OCCURRENCE ----
-
-# Co-occ of all 3 = very rare
-fractions |> 
-  summarise(n = n(), .by = richness) |>
-  mutate(n = n / sum(n))
-
-## plot ----
-
-fractions |>
-  filter(richness < 3) |>
-  mutate(sample = if_else(sample == 1, "spring", "summer")) |>
-  ggplot() +
-  theme_bw() +
-  scale_color_viridis_c(option="plasma", end=0.9) +
-  geom_point(aes(x=desiccation_dynamic, y=fraction, col=year)) + 
-  facet_grid(richness~sample, scales = "free", 
-             labeller = label_bquote(rows=paste("richness = ", .(richness)),
-                                     cols=paste(.(sample), " sample"))) +
-  aes(x=desiccation_dynamic, y=fraction, col=year, group=as.factor(year)) +
-  geom_smooth(lwd=0.5, method = lm, se=F, show.legend = F) +
-  labs(x = "nr of desiccation events (proxy for dispersal rate)", 
-       y = "fraction")
-
-ggsave(paste0("../figures/f_desiccation.pdf"), width=4.5, height = 3, 
-       device = "pdf")
+ggplot(counts_env) +
+  aes(x = desiccation_dynamic, y = pools_dens, 
+      col = as.factor(richness)) + 
+  geom_jitter() + 
+  facet_grid(.~sample, scales = "free", labeller = label_both)
+  
 
 ## stats ----
-data <- fractions |>
-  filter(richness < 3) |>
-  mutate(success = as.integer(fraction * n),
-         failure = n - as.integer(success),
-         year = as.factor(year)) |>
-  select(sample, richness, success, failure, desiccation_dynamic, year) |>
-  group_by(sample, richness) |>
-  nest() |>
-  ungroup() |>
-  mutate(model = map(data, ~ glm(cbind(success, failure) ~ desiccation_dynamic:year, 
-                                 family = binomial(link = "logit"), 
-                                 data = .x)),
-         summ = map(model, ~ tidy(.x)))
-
-# Pulex replaces magna as dispersal rates increase
-# I tried models with interactions between year and disp, but these led to only the interaction between both being significant and not the main effects
-# I also tried making year a factor, but this led to convergence issues (too many unknown parameters)
-
-# DOMINANCE ----
-## plot ----
-
-dom <- counts |>
-  mutate(richness = magna + longispina + pulex) |>
-  #remove empty pools
-  filter(richness > 0) |>
-  #number of pools evaluated in a given cluster at a given sampling event in a given year
-  mutate(n = length(unique(poolname)), 
-         .by = c(cluster, sample, year)) |> 
-  #remove all cluster - sampling event - year combos that have too small n
-  filter(n > 10) |>
-  mutate(`only magna` = (magna)*(1-longispina)*(1-pulex),
-         `only longi` = (1-magna)*(longispina)*(1-pulex),
-         `only pulex` = (1-magna)*(1-longispina)*(pulex)) |>
-  select(!magna & !longispina & !pulex & !water & !author & !richness) |>
-  pivot_longer(starts_with("only"), 
-               names_to = "species", values_to = "value") |>
-  #fraction of pools with only species x
-  summarize(fraction = sum(value) / mean(n), 
-            .by = c(cluster, sample, year, species, n)) |> #, "author"
-  #add desiccation data
-  left_join(desiccation, by = join_by(cluster, year)) |>
-  filter(!is.na(desiccation_dynamic))
-
-dom |>
-  mutate(sample = if_else(sample == 1, "spring", "summer")) |>
-  ggplot() +
-  theme_bw() +
-  scale_color_viridis_c(option="plasma", end=0.9) +
-  geom_point(aes(x=desiccation_dynamic, y=fraction, col=year)) +
-  facet_grid(species~sample, scales = "free", 
-             labeller = label_bquote(rows=paste(.(species)),
-                                     cols=paste(.(sample), " sample"))) +
-  aes(x=desiccation_dynamic, y=fraction, col=year, group=as.factor(year)) +
-  geom_smooth(lwd=0.5, method = lm, se=F, show.legend = F) +
-  labs(x = "nr of desiccation events (proxy for dispersal rate)", 
-       y = "fraction")
-
-ggsave(paste0("../figures/f_dom.pdf"), width=4.5, height = 4, 
-       device = "pdf")
-
-## stats ----
-data <- dom |> 
-  mutate(success = as.integer(fraction * n),
-         failure = n - as.integer(success)) |>
-  select(sample, year, species, success, failure, desiccation_dynamic, year) |>
-  group_by(sample, species) |>
-  nest() |>
-  ungroup() |>
-  mutate(model = map(data, ~ glm(cbind(success, failure) ~ desiccation_dynamic + year, 
-                                 family = binomial(link = "logit"), 
-                                 data = .x)),
-         summ = map(model, ~ tidy(.x)))
-# Pulex replaces magna as dispersal rates increase
+model <- glmer(richness ~ pH + pools_dens + 
+                 desiccation_dynamic + (1|cluster),
+               data = counts_env |> filter(sample == 2), 
+               family = poisson(link = "log"))
+# model fails to converge
+model <- glm(richness ~ pH + year + pools_dens + 
+                 desiccation_dynamic, 
+             data = counts_env |> filter(sample == 2), 
+             family = poisson(link = "log"))
+# model converges
+summary(model)
 
 # LEFTOVERS ----------
 
@@ -247,3 +148,8 @@ pH_conductivity <- read_csv("../data/Ebert/Frederik_data1/MetapopData_pH_conduct
   mutate(pH = (pH-mean(pH, na.rm = T))/sd(pH, na.rm = T),
          log_conduct = (log_conduct-mean(log_conduct, na.rm = T))/sd(log_conduct, na.rm = T)) 
 
+#location of pools
+pools           <- read_csv("../data/Ebert/Frederik_data1/Pools_coordinates_2017_vers7.csv") |>
+  left_join(island_measures, by="island") |>
+  select(island, pool, latitude_corr, longitude_corr, 
+         cluster, number_pools_approx, island_perimeter_meter, pools_dens) 
