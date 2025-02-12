f1ed2d62a48e814867b75aba9bbc59d5aed249ab
Frederik De Laender <frederik.delaender@unamur.be>
2025-02-12 10:25:46 +0100

    Prepared script for Olivia

--- code/daphnid-analysis.R
+++ code/daphnid-analysis.R
@@ -55,20 +55,80 @@ ggplot(counts_env) +
       col = as.factor(richness)) + 
   geom_jitter() + 
   facet_grid(.~sample, scales = "free", labeller = label_both)
-  
 
 ## stats ----
-model <- glmer(richness ~ pH + pools_dens + 
-                 desiccation_dynamic + (1|cluster),
+
+## save the data for olivia
+counts_env |> 
+  select(year, sample, island, pool, cluster, richness, pools_dens, desiccation_dynamic, pH) |>
+  saveRDS("../data/dataDaphnids.rds")
+  
+## load the data
+counts_env <- readRDS("../data/dataDaphnids.rds")
+# variables are: 
+# year: year of sampling
+# sample: spring (1), or summer sample (2)
+# island: unique island ID
+# pool: unique pool ID
+# cluster: unique cluster ID. A cluster is a collection of pools lying within a given distance from each other
+# richness: Daphnia species richness
+# pools_dens: number of pools per running m of a cluster's perimeter
+# desiccation_dynamic: estimates of pool desiccation rate
+# pH: measured pH
+
+# Check correlations among potential predictors of richness
+counts_env |> 
+  select(year, sample, pools_dens, desiccation_dynamic, pH) |>
+  cor(use = "pairwise.complete.obs")
+# desiccation_dynamic and year seem to be correlated and 
+# therefore I do not put them in the same model. 
+# It is well known that pools dry more often in recent years,
+# and so I assume that a year effect would mostly be a dryness effect.
+# I further drop pH from the list of predictors because we don't have 
+# pH measurements for 90% of the data. 
+# Finally, I focus on the summer sample because most representative. 
+
+# Fit the models 
+# Mixed GLM
+model1 <- glmer(richness ~ pools_dens + desiccation_dynamic + (1|cluster),
                data = counts_env |> filter(sample == 2), 
                family = poisson(link = "log"))
-# model fails to converge
-model <- glm(richness ~ pH + year + pools_dens + 
-                 desiccation_dynamic, 
+summary(model1)
+# Desiccation has positive effect on richness. No effect of pool density
+# But maybe driven by ponds at richness = 3?
+ggplot(counts_env |> filter(sample == 2)) + 
+  aes(y = as.factor(richness), x = desiccation_dynamic) + 
+  geom_boxplot()
+
+# Regular GLM
+model2 <- glm(richness ~ pools_dens + desiccation_dynamic, 
              data = counts_env |> filter(sample == 2), 
              family = poisson(link = "log"))
-# model converges
-summary(model)
+summary(model2)
+# Results are different: now pools density has positive effect, 
+# while desiccation has negative effect. Trends look dodgy though, 
+# and seem to be driven by data at richness = 2
+ggplot(counts_env |> filter(sample == 2)) + 
+  aes(y = as.factor(richness), x = pools_dens) + 
+  geom_boxplot()
+
+# Problem might be that there are only 31 observations with 
+# richness = 3 (out of the 30000). 
+# What happens if we remove these ponds? 
+# Mixed GLM
+model1 <- glmer(richness ~ pools_dens + desiccation_dynamic + (1|cluster),
+                data = counts_env |> filter(sample == 2, richness < 3), 
+                family = poisson(link = "log"))
+summary(model1)
+# Same result as before
+
+# Regular GLM
+model2 <- glm(richness ~ pools_dens + desiccation_dynamic, 
+              data = counts_env |> filter(sample == 2, richness < 3), 
+              family = poisson(link = "log"))
+summary(model2)
+# Same result as before
+
 
 # LEFTOVERS ----------
 
