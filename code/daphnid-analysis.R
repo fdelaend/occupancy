42f304b2d30de33ce00437cbb2ed8e1627ea3e29
Frederik De Laender <frederik.delaender@unamur.be>
2025-02-18 15:28:09 +0100

    Included nr of surrounding pools as predictor

--- code/daphnid-analysis.R
+++ code/daphnid-analysis.R
@@ -7,19 +7,37 @@ library(lme4)
 cluster_measures <- read_csv("../data/Ebert/Frederik_data1/Island_measures_vers4.csv") |>
   mutate(cluster = factor(group_100)) |>
   mutate(nr_pools = sum(number_pools_approx),
-            area = sum(island_area_sq_meter),
-            peri = sum(island_perimeter_meter),
-            .by = cluster) |>
+         area = sum(island_area_sq_meter),
+         peri = sum(island_perimeter_meter),
+         .by = cluster) |>
   mutate(pools_dens = nr_pools / peri) |>
-  select(cluster, island, nr_pools, peri, pools_dens)
+  select(cluster, island, pools_dens) 
+
+pools <- read_csv("../data/Ebert/Frederik_data1/Pools_coordinates_2017_vers7.csv") |>
+  filter(!grepl("A", pool), !grepl("A", name)) |> #island present twice in the data
+  select(island, pool, latitude_corr, longitude_corr) |>
+  left_join(cluster_measures |> select(cluster, island), 
+            by = "island") |>
+  group_by(cluster) |>
+  nest() |>
+  mutate(distances = map(data, ~ tidy(dist(.x |> select(latitude_corr, longitude_corr), upper = T)))) |>
+  expand_grid(d = c(0.0001, 0.0005, 0.001, 0.002)) |>
+  # nr of pools within 0.001 distance
+  mutate(nr_pools_within = map2(distances, d, ~ .x |> 
+                                 summarise(nr = sum((distance < .y)), .by = c(item1)))) |>
+  # glue these results to the data
+  mutate(data = map2(data, nr_pools_within, ~ .x |> mutate(nr_pools_within = .y$nr))) |>
+  select(cluster, d, data) |>
+  unnest(data)
 
 counts          <- read_csv("../data/Ebert/Frederik_data1/Daphnia_dynamics_1982_2017_2.csv") |>
-  left_join(cluster_measures, by="island")
+  filter(!grepl("A", pool), !grepl("A", poolname)) |> #islands present twice in the data
+  remove_missing() |>
+  left_join(cluster_measures, by = join_by(island)) |>
+  left_join(pools, by = join_by(cluster, island, pool))
 
-desiccation_static <- read_csv("../data/Ebert/Frederik_data1/Data_hydroperiod_Means.csv") |>
-  expand_grid(year = c(min(counts$year):max(counts$year)))
-
-desiccation        <- desiccation_static |>
+desiccation        <- read_csv("../data/Ebert/Frederik_data1/Data_hydroperiod_Means.csv") |>
+  expand_grid(year = c(min(counts$year):max(counts$year))) |>
   left_join(read_csv("../data/Ebert/hydro.csv"), 
             by = join_by(poolname == pool, year)) |> 
   separate_wider_delim(poolname, delim = "-", names = c("island", "pool")) |>
@@ -31,13 +49,6 @@ desiccation        <- desiccation_static |>
             desiccation_static_median_mean = median(meandesi, na.rm = T),
             .by = c(year, cluster))
 
-#environmental variables
-pH_conductivity <- read_csv("../data/Ebert/Frederik_data1/MetapopData_pH_conductivity_1998_2017.csv") |>
-  # compute mean data across sampling occasions for every pool 
-  summarize(pH = log10(mean(10^pH, na.rm = T)), 
-            log_conduct = mean(log10(conduct_uS), na.rm = T), 
-            .by = c("year", "island", "pool"))
-
 # add environmental data to count data
 counts_env <- counts |>
   mutate(richness = magna + longispina + pulex) |>
@@ -45,90 +56,57 @@ counts_env <- counts |>
   mutate(year_lag = year - 1) |>
   #add desiccation data
   left_join(desiccation, by = join_by(cluster, year_lag == year)) |>
-  #add pH and cond 
-  left_join(pH_conductivity, by = join_by(island, pool, year)) |>
   #remove na-s
-  filter(!is.na(richness))
-
-ggplot(counts_env) +
-  aes(x = desiccation_dynamic, y = pools_dens, 
-      col = as.factor(richness)) + 
-  geom_jitter() + 
-  facet_grid(.~sample, scales = "free", labeller = label_both)
-
-## stats ----
+  filter(!is.na(richness)) |>
+  mutate(sample = if_else(sample == 1, "spring", "summer")) |> 
+  filter(richness >0)
 
 ## save the data for olivia
 counts_env |> 
-  select(year, sample, island, pool, cluster, richness, pools_dens, desiccation_dynamic, pH) |>
+  select(year, sample, island, pool, cluster, 
+         richness, pools_dens, desiccation_dynamic, 
+         latitude_corr, longitude_corr, nr_pools_within) |>
   saveRDS("../data/dataDaphnids.rds")
-  
+
+ggplot(counts_env) +
+  theme_bw() +
+  scale_color_viridis_d(option="plasma", end=0.9) +
+  aes(x = nr_pools_within, y = desiccation_dynamic, 
+      col = as.factor(richness)) + 
+  geom_point() + 
+  facet_grid(d~sample, scales = "free", labeller = label_value) +
+  labs(x="nr of surrounding rockpools", y = "desiccation rate", 
+       col="richness")
+
+## stats ----
 ## load the data
 counts_env <- readRDS("../data/dataDaphnids.rds")
 # variables are: 
 # year: year of sampling
-# sample: spring (1), or summer sample (2)
+# sample: spring or summer sample
 # island: unique island ID
 # pool: unique pool ID
 # cluster: unique cluster ID. A cluster is a collection of pools lying within a given distance from each other
 # richness: Daphnia species richness
 # pools_dens: number of pools per running m of a cluster's perimeter
 # desiccation_dynamic: estimates of pool desiccation rate
-# pH: measured pH
-
-# Check correlations among potential predictors of richness
-counts_env |> 
-  select(year, sample, pools_dens, desiccation_dynamic, pH) |>
-  cor(use = "pairwise.complete.obs")
-# desiccation_dynamic and year seem to be correlated and 
-# therefore I do not put them in the same model. 
-# It is well known that pools dry more often in recent years,
-# and so I assume that a year effect would mostly be a dryness effect.
-# I further drop pH from the list of predictors because we don't have 
-# pH measurements for 90% of the data. 
-# Finally, I focus on the summer sample because most representative. 
-
-# Fit the models 
-# Mixed GLM
-model1 <- glmer(richness ~ pools_dens + desiccation_dynamic + (1|cluster),
-               data = counts_env |> filter(sample == 2), 
-               family = poisson(link = "log"))
-summary(model1)
-# Desiccation has positive effect on richness. No effect of pool density
-# But maybe driven by ponds at richness = 3?
-ggplot(counts_env |> filter(sample == 2)) + 
-  aes(y = as.factor(richness), x = desiccation_dynamic) + 
-  geom_boxplot()
+# latitude_corr and longitude_corr: lat and long
+# nr_pools_within: nr of pools within a certain distance
 
 # Regular GLM
-model2 <- glm(richness ~ pools_dens + desiccation_dynamic, 
-             data = counts_env |> filter(sample == 2), 
+model <- glm(richness ~ nr_pools_within*sample + desiccation_dynamic*sample, 
+             data = counts_env |> filter(d == 0.001, richness >0), 
              family = poisson(link = "log"))
-summary(model2)
-# Results are different: now pools density has positive effect, 
-# while desiccation has negative effect. Trends look dodgy though, 
-# and seem to be driven by data at richness = 2
-ggplot(counts_env |> filter(sample == 2)) + 
-  aes(y = as.factor(richness), x = pools_dens) + 
-  geom_boxplot()
+summary(model)
 
-# Problem might be that there are only 31 observations with 
-# richness = 3 (out of the 30000). 
-# What happens if we remove these ponds? 
-# Mixed GLM
-model1 <- glmer(richness ~ pools_dens + desiccation_dynamic + (1|cluster),
-                data = counts_env |> filter(sample == 2, richness < 3), 
-                family = poisson(link = "log"))
-summary(model1)
-# Same result as before
-
-# Regular GLM
-model2 <- glm(richness ~ pools_dens + desiccation_dynamic, 
-              data = counts_env |> filter(sample == 2, richness < 3), 
-              family = poisson(link = "log"))
-summary(model2)
-# Same result as before
-
+# Inspect residuals
+counts_env |>
+  mutate(pred = predict(model, newdata = counts_env, type = "response"),
+         res = (pred - richness)^2) |>
+  ggplot() +
+  aes(x = latitude_corr, y = longitude_corr, col = res) +
+  geom_point() +
+  facet_grid(.~sample, scales = "free", labeller = label_both)
 
 # LEFTOVERS ----------
 
@@ -213,3 +191,44 @@ pools           <- read_csv("../data/Ebert/Frederik_data1/Pools_coordinates_2017
   left_join(island_measures, by="island") |>
   select(island, pool, latitude_corr, longitude_corr, 
          cluster, number_pools_approx, island_perimeter_meter, pools_dens) 
+
+# Fit the models 
+# Resample 10 pools per cluster
+resampled_data <- counts_env |> 
+  group_by(cluster, year) |>
+  nest() |>
+  expand_grid(iteration = c(1:100)) |>
+  mutate(data_sample = map(data, ~ .x[sample(1:nrow(.x), 12),])) |>
+  select(!data) |>
+  unnest(data_sample) |>
+  group_by(iteration) |>
+  nest() |>
+  mutate(model = map(data, ~ tidy(glm(richness ~ pools_dens + desiccation_dynamic, 
+                                      data = .x, 
+                                      family = poisson(link = "log"))))) |>
+  select(!data) |>
+  unnest(model) |>
+  filter(term != "(Intercept)")
+
+ggplot(resampled_data) +
+  aes(col = term, y = estimate) +
+  geom_density()
+
+#environmental variables
+pH_conductivity <- read_csv("../data/Ebert/Frederik_data1/MetapopData_pH_conductivity_1998_2017.csv") |>
+  # compute mean data across sampling occasions for every pool 
+  summarize(pH = log10(mean(10^pH, na.rm = T)), 
+            log_conduct = mean(log10(conduct_uS), na.rm = T), 
+            .by = c("year", "island", "pool"))
+
+# Check correlations among potential predictors of richness
+counts_env |> 
+  select(year, sample, nr_pools_within, desiccation_dynamic) |>
+  cor(use = "pairwise.complete.obs")
+# desiccation_dynamic and year seem to be correlated and 
+# therefore I do not put them in the same model. 
+# It is well known that pools dry more often in recent years,
+# and so I assume that a year effect would mostly be a dryness effect.
+# I further drop pH from the list of predictors because we don't have 
+# pH measurements for 90% of the data. 
+# Finally, I focus on the summer sample because most representative. 
