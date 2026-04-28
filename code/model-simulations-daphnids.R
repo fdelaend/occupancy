117731cc28c8e87f46a9cc8be65fc9acb92cbaa2
Frederik De Laender <frederik.delaender@unamur.be>
2026-04-28 15:38:14 +0200

    specify new folder to save simulations

--- code/model-simulations-daphnids.R
+++ code/model-simulations-daphnids.R
@@ -49,5 +49,5 @@ results <- Sims %>%
 
 ## Save results ----
 write_rds(results, 
-          file=str_c("../simulated-data/", 
+          file=str_c("../simulated-data-daphnids/", 
                      parallel_id,"data.RDS"))
