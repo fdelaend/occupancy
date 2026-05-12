18db5055cf1b949982c08e782d1e97a26fcdf445
Frederik De Laender <frederik.delaender@unamur.be>
2026-05-12 18:01:24 +0200

    update of daphnid simulation plot

--- code/model-analysis-daphnids.R
+++ code/model-analysis-daphnids.R
@@ -16,40 +16,37 @@ Sims <- readRDS(file="../simulated-data/simulated-data-daphnids/all-data.RDS")
 # Summarize simulated data -----
 SimsSum <- Sims |> 
   select(all_of(c("n", "meanA", "d", "vary", "k", 
-                  "cvA", "p", "dispType", "summaryM", "propPatchesN", "propPatches2"))) |>
+                  "cvA", "p", "dispType", "summaryM", 
+                  "propPatchesN", "propPatches2"))) |>
   summarise(meanProb = mean(propPatchesN, na.rm = T), 
             sdProb = sd(propPatchesN, na.rm = T), 
             meanProb2 = mean(propPatches2, na.rm = T), 
             sdProb2 = sd(propPatches2, na.rm = T), 
-            .by = c(n, meanA, d, vary, k, cvA, p, dispType))# |>
-  #mutate(k = if_else(k==1, "Equivalence", "Inequivalence"))
+            .by = c(n, meanA, d, vary, k, cvA, p, dispType)) |>
+  mutate(k = if_else(k==1, "Equivalence", paste("Inequivalence, k = ", k)))
+
+# Only select realistic parameter settings ; ------
+# These are those for which meanProb < 0.01 across all values of dispersal 
+SimsSum_realistic <- SimsSum |>
+  group_by(d, meanA, vary, k, cvA, dispType) |>
+  filter(all(meanProb < 0.01)) |>
+  ungroup()
 
 #Plot -----
-# choice of d range based on Dubart. Max of about 10e-4. 
-# Only when d gets at the higher end: p has negative effect.
-ggplot(SimsSum |>
-         filter(meanA == 1.2, cvA == 0.01, vary == 0.1)) + 
+ggplot(SimsSum_realistic |> 
+         filter(dispType == "exponentialD")) + # "exponentialD"
   theme_bw() +
-  scale_colour_gradient(low = "grey85", high = "black") +
+  scale_colour_viridis_c(option="plasma", end=0.9) +
   theme(panel.grid = element_blank()) +
-  geom_line(aes(x=p, y=meanProb2, col = log10(d),
-                group = log10(d))) +
-  facet_grid(k~dispType) +
-  labs(col=expression(paste("dispersal rate, log"[10],"(d)")), 
-       x="nr. of patches, p", y="patch occupancy")
+  aes(x=p, y=meanProb2, col = meanA) +
+  geom_point() +
+  facet_grid(k~cvA, 
+             labeller = label_bquote(cols = paste(cv(a), " = ", .(cvA)))) + #k, cvA, dispType
+  #geom_smooth(se=FALSE) +
+  labs(col=expression(paste(bar(a))), 
+       x="nr. of patches, p", y="patch occupancy of pairs when n=3")
 
 ggsave(paste0("../figures/patch-occupancy-daphnids.pdf"), 
-       width=6, height = 3, device = "pdf")  
+       width=6, height = 6, device = "pdf")  
 
-ggplot(SimsSum |>
-         filter(vary > 0, cvA == 0.2, 
-                log10(d) > -6, log10(d)< -4)) + 
-  theme_bw() +
-  scale_colour_gradient(low = "grey85", high = "black") +
-  theme(panel.grid = element_blank()) +
-  geom_line(aes(x=log10(d), y=meanProb2, col = p,
-                group = p)) +
-  facet_grid(k~dispType) +
-  labs(x=expression(paste("dispersal rate, log"[10],"(d)")), 
-       col="nr. of patches, p", y="patch occupancy")
 
